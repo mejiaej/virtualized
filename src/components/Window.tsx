376205e2e43ed72f61297b040803b255edd03f5f
Enrique <mejiaej@gmail.com>
2023-02-23 22:52:01 -0500

    feat: render visible children

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,14 +1,15 @@
 import { throttle } from "lodash";
-import { useEffect, useRef, useState } from "react";
+import { cloneElement, useEffect, useRef, useState } from "react";
 
 interface WindowProps {
   rowHeight: number;
   children: Array<JSX.Element>;
 }
 
+const bufferedItems = 2;
 const Window: React.FC<WindowProps> = ({ rowHeight, children }) => {
-  const [scrollPosition, setScrollPosition] = useState<number>();
-  const [containerHeight, setContainerHeight] = useState<number>();
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [containerHeight, setContainerHeight] = useState<number>(0);
   const containerRef = useRef<HTMLUListElement>(null);
 
   const throttledOnSroll = throttle(
@@ -24,18 +25,43 @@ const Window: React.FC<WindowProps> = ({ rowHeight, children }) => {
   };
 
   useEffect(() => {
-    setContainerHeight(containerRef?.current?.clientHeight);
+    setContainerHeight(containerRef?.current?.clientHeight || 0);
   }, []);
+
+  const renderVisibleChildren = () => {
+    const startIndex = Math.max(
+      Math.floor(scrollPosition / rowHeight) - bufferedItems,
+      0
+    );
+    const endIndex =
+      Math.ceil((scrollPosition + containerHeight) / rowHeight) -
+      1 +
+      bufferedItems;
+
+    return children.slice(startIndex, endIndex + 1).map((child, index) =>
+      cloneElement(child, {
+        style: {
+          position: "absolute",
+          top: (startIndex + index) * rowHeight,
+          left: 50,
+          right: 50,
+          height: rowHeight,
+        },
+      })
+    );
+  };
 
   console.log("scrollPosition", scrollPosition);
   console.log("containerHeight", containerHeight);
+  const content = renderVisibleChildren();
+  console.log("content.length", content.length);
   return (
     <ul
       ref={containerRef}
       onScroll={handleOnScroll}
-      style={{ overflowY: "scroll", height: "100%" }}
+      style={{ overflowY: "scroll", height: "100%", position: "relative" }}
     >
-      {children}
+      {renderVisibleChildren()}
     </ul>
   );
 };
